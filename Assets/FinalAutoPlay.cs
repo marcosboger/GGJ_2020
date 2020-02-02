1e38c9cb52df18fd7a3c77a310142b0f01c83e10
LucaRoja <49695464+LucaRoja@users.noreply.github.com>
2020-02-02 09:50:18 -0300

    Enemy on 4, change on 5, final screen

---
+++ Assets/FinalAutoPlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalAutoPlay : MonoBehaviour
+{
+    [SerializeField] private float waitToPlay = 1f;
+    private GameObject TileManager;
+    private GameObject TimeManager;
+    private bool alreadyPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeManager = GameObject.Find("Game Canvas");
+        TileManager = GameObject.Find("Tile Manager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!alreadyPlaying)
+        {
+            alreadyPlaying = true;
+            TimeManager.GetComponent<TimeManager>().StartTime();
+            TileManager.GetComponent<TilemapBehaviour>().deactivateChanges();
+        }
+        
+    }
+}
