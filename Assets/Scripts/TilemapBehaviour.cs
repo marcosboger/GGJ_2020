3d5d871e80e49063c5e14f01960a561c37cb4b85
Marcos Boger <marcos.boger@usp.br>
2020-02-02 02:03:47 +0100

    Blue Highlight around interactable blocks

--- Assets/Scripts/TilemapBehaviour.cs
+++ Assets/Scripts/TilemapBehaviour.cs
@@ -7,7 +7,7 @@ public class TilemapBehaviour : MonoBehaviour
 {
     public Tilemap interactable, interactableNonTrigger, highlightTilemap;
     private Tile firstClickTile, secondClickTile;
-    public Tile highlight;
+    public Tile highlightGreen, highlightBlue;
     private bool firstTileTrigger, secondTileTrigger;
     private Vector3Int firstClickPos, secondClickPos;
     private bool _first = false;
@@ -19,12 +19,13 @@ public class TilemapBehaviour : MonoBehaviour
     private GameObject[] enemies;
     private void Start()
     {
-        interactable.color = Color.yellow;
-        interactableNonTrigger.color = Color.yellow;
+        //interactable.color = Color.yellow;
+        //interactableNonTrigger.color = Color.yellow;
         firstTileTrigger = true;
         secondTileTrigger = true;
         //_player = GameObject.Find("Player_80"); // Use the Serializable field, otherwise we can't use the old player prefab anymore
         _initialPosition = new Vector2(_player.transform.position.x, _player.transform.position.y);
+        fillHighlightTile();
     }
 
     public void activateChanges()
@@ -32,8 +33,8 @@ public class TilemapBehaviour : MonoBehaviour
         _stop = true;
         _first = false;
         _player.transform.position = new Vector2(_initialPosition.x, _initialPosition.y);
-        interactable.color = Color.yellow;
-        interactableNonTrigger.color = Color.yellow;
+        //interactable.color = Color.yellow;
+        //interactableNonTrigger.color = Color.yellow;
         _player.GetComponent<Player>().direction = 1f;
         enemies = GameObject.FindGameObjectsWithTag("Death");
         foreach(GameObject e in enemies)
@@ -53,6 +54,7 @@ public class TilemapBehaviour : MonoBehaviour
                 }
             }
         }
+        fillHighlightTile();
     }
 
     public void deactivateChanges()
@@ -62,8 +64,8 @@ public class TilemapBehaviour : MonoBehaviour
         interactable.GetComponent<TilemapCollider2D>().enabled = true;
         interactableNonTrigger.GetComponent<TilemapCollider2D>().enabled = false;
         interactableNonTrigger.GetComponent<TilemapCollider2D>().enabled = true;
-        interactable.color = Color.white;
-        interactableNonTrigger.color = Color.white;
+        //interactable.color = Color.white;
+        //interactableNonTrigger.color = Color.white;
         foreach (var pos in interactable.cellBounds.allPositionsWithin)
         {
   
@@ -76,6 +78,7 @@ public class TilemapBehaviour : MonoBehaviour
                 }
             }
         }
+        unfillHighlightTile();
     }
 
     private void Update()
@@ -109,11 +112,11 @@ public class TilemapBehaviour : MonoBehaviour
                     interactableNonTrigger.SetTileFlags(firstClickPos, TileFlags.None);
                     //interactableNonTrigger.SetColor(firstClickPos, Color.green);
                 }
-                highlightTilemap.SetTile(firstClickPos, highlight);
+                highlightTilemap.SetTile(firstClickPos, highlightGreen);
             }
             else if(_stop)
             {
-                highlightTilemap.SetTile(firstClickPos, null);
+                highlightTilemap.SetTile(firstClickPos, highlightBlue);
                 _first = false;
                 secondClickPos = interactable.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
                 secondClickTile = interactable.GetTile<Tile>(secondClickPos);
@@ -160,6 +163,41 @@ public class TilemapBehaviour : MonoBehaviour
                 //interactable.SetColor(secondClickPos, Color.white);
                 //interactableNonTrigger.SetColor(firstClickPos, Color.white);
                 //interactableNonTrigger.SetColor(secondClickPos, Color.white);
+            }
+        }
+    }
+    private void fillHighlightTile()
+    {
+        foreach (var pos in interactable.cellBounds.allPositionsWithin)
+        {
+            if (interactable.GetTile<Tile>(pos) != null)
+            {
+                highlightTilemap.SetTile(pos, highlightBlue);
+            }
+        }
+        foreach (var pos in interactableNonTrigger.cellBounds.allPositionsWithin)
+        {
+            if (interactableNonTrigger.GetTile<Tile>(pos) != null)
+            {
+                highlightTilemap.SetTile(pos, highlightBlue);
+            }
+        }
+    }
+
+    private void unfillHighlightTile()
+    {
+        foreach (var pos in interactable.cellBounds.allPositionsWithin)
+        {
+            if (interactable.GetTile<Tile>(pos) != null)
+            {
+                highlightTilemap.SetTile(pos, null);
+            }
+        }
+        foreach (var pos in interactableNonTrigger.cellBounds.allPositionsWithin)
+        {
+            if (interactableNonTrigger.GetTile<Tile>(pos) != null)
+            {
+                highlightTilemap.SetTile(pos, null);
             }
         }
     }
